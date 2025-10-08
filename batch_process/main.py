307fee7d24157e9848935269e93ab2b89b7b4473
vashishthaharishankar <vashishthaharishankar@gmail.com>
2025-10-09 02:39:10 +0530

    fixed startup bug

--- batch_process/main.py
+++ batch_process/main.py
@@ -1,6 +1,7 @@
 import logging
 
-import steps
+from . import steps
+
 
 def batch_processing_create_file(user_id, filename, df, description_json):
     output_df = steps.create_file.start_process(df, description_json["unique_id_field"])
@@ -8,17 +9,10 @@ def batch_processing_create_file(user_id, filename, df, description_json):
     # steps.start_batch()
     # steps.download_file()
 
-    return {
-        "message": "Hello",
-        "length": len(output_df)
-    }
-
+    return {"message": "Hello", "length": len(output_df)}
 
 
 def batch_processing_upload_file(user_id, filename, df, description_json):
     file_ids_list = steps.upload_file.start_process(df, filename, description_json)
 
-    return {
-        "message": "Hello",
-        "file_ids": file_ids_list
-    }
+    return {"message": "Hello", "file_ids": file_ids_list}
